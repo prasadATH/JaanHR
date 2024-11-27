ec1ae762b425b74b5e4d1f59760f5a004af02301
Thilina Prasad Athukorala <thilinaprasad603@gmail.com>
2024-11-27 11:15:49 +0530

    Initial css

---
+++ CSS/index.php
@@ -0,0 +1,8 @@
+<?php include("./includes/header.php"); ?>
+<?php include("./includes/log_nav.php"); ?>
+<?php include("./login.php"); ?>
+<?php include("./sign.php"); ?>
+<?php include("./password-1.php"); ?>
+<?php include("./password-2.php"); ?>
+<?php include("./su.php"); ?>
+<?php include("./includes/footer.php"); ?>
